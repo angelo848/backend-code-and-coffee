9e39e7b957d9c14e1ea071b9fca5d78f8c002928
Angelo Salles <angelo_848@live.com>
2020-01-20 21:06:31 -0300

    database configuration ok

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'code_and_coffee',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}

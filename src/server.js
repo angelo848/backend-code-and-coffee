b66d9a6b5320f701f7057b0d9c3f95405d173688
Angelo Salles <angelo_848@live.com>
2020-01-21 18:05:36 -0300

    removed console log from server

--- src/server.js
+++ src/server.js
@@ -12,6 +12,3 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333)
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)

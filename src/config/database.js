12f0cd0ed232e4ea2f88e93d4be5d0a3d5af7ce8
Angelo Salles <angelo_848@live.com>
2020-01-21 14:30:16 -0300

    Config of database server

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,22 @@
+// DEVELOPMENT DB
+// module.exports = {
+//   dialect: 'mysql',
+//   host: 'localhost',
+//   username: 'root',
+//   password: '',
+//   database: 'code_and_coffee',
+//   define: {
+//     timestamps: true,
+//     underscored: true
+//   }
+// }
+
+// PRODUCTION DB
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: '',
-  database: 'code_and_coffee',
-  define: {
-    timestamps: true,
-    underscored: true
-  }
+  host:
+    'mysql://bc13c138ab156a:9dc84451@us-cdbr-iron-east-05.cleardb.net/heroku_46fa3a170055a33?reconnect=true',
+  username: 'bc13c138ab156a',
+  password: '9dc84451',
+  database: 'heroku_46fa3a170055a33'
 }

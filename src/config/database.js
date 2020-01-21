ffaf31c6a9bd18fc30bb077d7207b885618a0494
Angelo Salles <angelo_848@live.com>
2020-01-21 14:33:00 -0300

    fix db

--- src/config/database.js
+++ src/config/database.js
@@ -14,8 +14,7 @@
 // PRODUCTION DB
 module.exports = {
   dialect: 'mysql',
-  host:
-    'mysql://bc13c138ab156a:9dc84451@us-cdbr-iron-east-05.cleardb.net/heroku_46fa3a170055a33?reconnect=true',
+  host: process.env.CLEARDB_DATABASE_URL,
   username: 'bc13c138ab156a',
   password: '9dc84451',
   database: 'heroku_46fa3a170055a33'

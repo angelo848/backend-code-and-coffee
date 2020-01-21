be17c9675a085db3172a1c47eabca05764e01b6e
Angelo Salles <angelo_848@live.com>
2020-01-20 23:41:14 -0300

    UserController finished

---
+++ src/controllers/UserController.js
@@ -0,0 +1,60 @@
+const User = require('../models/User')
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const { id } = req.params
+      const user = await User.findByPk(id)
+
+      return res.json(user)
+    } catch (error) {
+      return res.status(500).send(error.toString())
+    }
+  },
+
+  async store(req, res) {
+    try {
+      const { name, user_name, email, password } = req.body
+      const user = await User.create({
+        name,
+        user_name,
+        email,
+        password
+      })
+
+      return res.json(user)
+    } catch (error) {
+      return res.status(500).send(error.toString())
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const { id } = req.params
+      const { name, user_name, email, password } = req.body
+      const user = await User.findByPk(id)
+      await user.update({
+        name,
+        user_name,
+        email,
+        password
+      })
+
+      return res.json(user)
+    } catch (error) {
+      return res.status(500).send(error.toString())
+    }
+  },
+
+  async destroy(req, res) {
+    try {
+      const { id } = req.params
+      const user = await User.findByPk(id)
+      await user.destroy()
+
+      return res.json(user)
+    } catch (error) {
+      return res.status(500).send(error.toString())
+    }
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const routes = express.Router()
+const UserController = require('./controllers/UserController')
 
-routes.get('/', (req, res) => {
-  res.json({ hello: 'World' })
-})
+routes.get('/users/:id', UserController.index)
+routes.post('/users', UserController.store)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.destroy)
 
 module.exports = routes
